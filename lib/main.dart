97c9d77221f485e045ffd0fb1acb3c796bc49f84
form_app <subbublockchaindoveloper@gmail.com>
2023-11-04 16:51:09 +0530

    initial commint changes to firebase configure to linl ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,71 +1,113 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
+import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 
 void main(){
-  runApp(const LogOutScreen());
+  runApp(
+      const LogOutScreen());
 }
 
-class LogOutScreen extends StatelessWidget {
+
+class LogOutScreen extends StatefulWidget {
   const LogOutScreen({super.key});
 
   @override
+  State<LogOutScreen> createState() => _LogOutScreenState();
+}
+
+class _LogOutScreenState extends State<LogOutScreen> {
+  @override
   Widget build(BuildContext context) {
+    return  MaterialApp(
 
-    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Center(child: Text('Firebase Mettup')),
+          backgroundColor: Colors.deepPurple,
+        ),
+        body: Column(
 
-    home: Scaffold(
-       appBar: AppBar(
-         title: Center(child: Text('Firebase Mettup')),
-         backgroundColor: Colors.deepPurple,
-       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
 
-          Image.asset('images/firebasemeetup.png',
-            // width: double.infinity,
-            fit: BoxFit.fill,
-            height: 250,
-            width: double.infinity,
+          children: [
 
-          ),
+            Image.asset('images/firebasemeetup.png',
+              // width: double.infinity,
+              fit: BoxFit.fill,
+              height: 250,
+              width: double.infinity,
 
-          Row(
-
-            children: [
-              Padding(padding: EdgeInsets.symmetric(horizontal: 5,vertical: 15)),
-              Icon(Icons.calendar_today),
-              Text('October 30'),
-            ],
-          ),
-          Row(
-            children: [
-              Padding(padding: EdgeInsets.symmetric(horizontal: 5,vertical: 15)),
-              Icon(Icons.location_city),
-              Text('San Francisco'),
-            ],
-          ),
-          Padding(padding: EdgeInsets.symmetric(horizontal: 5,vertical: 15)),
-          OutlinedButton(
-            style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all<Color>(Colors.white)
             ),
-              onPressed: (){
 
-          }, child: Text('LOGOUT',style: TextStyle(color: Colors.black),)),
+            const Row(
+              children: [
+                Padding(
+                    padding: EdgeInsets.symmetric(
+                        horizontal: 5,
+                        vertical: 15
+                    )
+                ),
+                Icon(Icons.calendar_today),
+                Text('October 30'),
+              ],
+            ),
 
-          TextFormField(
-            decoration: InputDecoration(),
-          ),
+            const Row(
+              children: [
+                Padding(
+                    padding: EdgeInsets.symmetric(
+                        horizontal: 5,
+                        vertical: 15
+                    )
+                ),
+                Icon(Icons.location_city),
+                Text('San Francisco'),
+              ],
+            ),
 
-          Text('What we\'ll be doing',style: TextStyle(fontSize: 30,color: Colors.black),),
+            const Padding(
+                padding: EdgeInsets.symmetric(
+                    horizontal: 5,
+                    vertical: 10
+                )
+            ),
 
-          Text('Join us for a day full of Firebase Workshops\n and Pizza!',style: TextStyle(fontSize: 15),),
-        ],
+            OutlinedButton(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                ),
+                onPressed: (){
+
+                }, child: const Text('LOGOUT',
+              style: TextStyle(color: Colors.black),
+            )
+            ),
+
+            TextFormField(
+              decoration: InputDecoration(),
+            ),
+
+            const Text('What we\'ll be doing',
+              style: TextStyle(
+                  fontSize: 30,color: Colors.black
+              ),
+            ),
+
+            const Text('Join us for a day full of Firebase Workshops\n and Pizza!',
+              style: TextStyle(fontSize: 15),
+            ),
+          ],
+
+        ),
 
       ),
-
-    ),
     );
   }
 }
+
